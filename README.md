d0e939c627b607b0fc14204292a6811de76107d4
sam <sam@pop-os.localdomain>
2020-10-08 10:30:15 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# linux-battery-monitor
+A simple shell script for monitoring laptop battery percentage 
---
+++ battery-monitor-cron.sh
@@ -0,0 +1,19 @@
+#!/bin/zsh
+
+battery_percentage=$((acpi -b | grep -P -o '[0-9]+(?=%)') | sort |  tail  -1)
+charger_online=$(acpi -b | grep -c "Charging")
+if [[ $charger_online -eq 1 && $battery_percentage -gt 85 ]]
+then
+    export DISPLAY=:0.0
+    notify-send -i "$PWD/battery_full.png" "Battery CHARGED" "Level: ${battery_percentage}%";
+    espeak "85% Charged, Please remove the adapter" -s 150
+fi
+
+if [[ $charger_online -eq 0 && $battery_percentage -lt 35 ]]
+then
+    export DISPLAY=:0.0
+    notify-send -i "$PWD/battery_low.png" "Battery LOW" "Level: ${battery_percentage}%";
+    espeak "Please Connect the Adapter" -s 150
+fi
+
+sleep 30 # (1 minutes)
